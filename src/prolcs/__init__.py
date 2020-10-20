Fix variance vs. standard deviation in normals

src/prolcs/__init__.py
@@ -531,7 +531,9 @@ def train_mixing(M: np.ndarray, X: np.ndarray, Y: np.ndarray, Phi: np.ndarray,
     N, D_Y = Y.shape
     N, D_V = Phi.shape
 
-    V = rng.normal(loc=0, scale=A_BETA / B_BETA, size=(D_V, K))
+    # Drugowitsch gives variance as argument to normal, but NumPy wants standard
+    # deviation, thus we ``sqrt``.
+    V = rng.normal(loc=0, scale=np.sqrt(A_BETA / B_BETA), size=(D_V, K))
     a_beta = np.repeat(A_BETA, K)
     b_beta = np.repeat(B_BETA, K)
     L_M_q = -np.inf

src/prolcs/problem/drugowitsch/generated_function.py
@@ -49,7 +49,10 @@ def generate(n: int = 300, rng: np.random.Generator = np.random.default_rng()):
         for k in range(len(ms)):
             # sample the three classifiers
             y += rng.normal(loc=G[n][k] * (W[k] @ X_[n]),
-                                  scale=Lambda_1[k])
+                                  # Drugowitsch gives variance as argument to
+                                  # normal, but NumPy wants standard deviation,
+                                  # thus we ``sqrt``.
+                                  scale=np.sqrt(Lambda_1[k])
         Y[n] = y
 
     # We return the non-augmented samples (because our algorithm augments them

src/prolcs/radialmatch.py
@@ -44,6 +44,7 @@ class RadialMatch():
         ranges: np.ndarray,
         lambd_2_gen: Callable[
             [np.ndarray],
+            # TODO Extract this
             np.ndarray] = lambda r: np.repeat(100, r.shape[0]**2).reshape(
                 (r.shape[0], r.shape[0])),
         rng: np.random.Generator = np.random.default_rng()):
@@ -84,7 +85,9 @@ class RadialMatch():
             self.rng.normal(
                 loc=self.mu,
                 # TODO This was chosen to be similar to [PDF p. 228] but isn't
-                # probably
+                # probably.
+                # TODO Drugowitsch gives variance, but NumPy wants standard
+                # deviation.
                 scale=0.01 * np.sum(self.ranges * np.array([-1, 1]), 1)),
             # clip to each dimension's range
             self.ranges[:, [0]].reshape((-1)),
@@ -93,6 +96,8 @@ class RadialMatch():
             loc=self.lambd_2,
             # TODO This was chosen relatively arbitrary (but motivated by [PDF
             # p. 228])
+            # TODO Drugowitsch gives variance, but NumPy wants standard
+            # deviation.
             scale=0.005 * np.sum(self.ranges * np.array([-1, 1]), 1),
             size=self.lambd_2.shape)
         return self