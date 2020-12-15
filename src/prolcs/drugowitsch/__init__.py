Correct KL(R||G) <= 0 assertion

src/prolcs/drugowitsch/__init__.py
@@ -411,12 +411,12 @@ def train_mix_weights(M: np.ndarray, X: np.ndarray, Y: np.ndarray,
             KLRG = np.sum(
                 R * np.nan_to_num(np.log(G / R), nan=0, posinf=0, neginf=0))
         # This fixes(?) some numerical problems.
-        if KLRG < 0 and np.isclose(KLRG, 0):
+        if KLRG > 0 and np.isclose(KLRG, 0):
             print(
                 "Warning: Kullback-Leibler divergence ever so slightly less than zero, fixing"
             )
             KLRG = 0
-        assert KLRG >= 0, f"Kullback-Leibler divergence less than zero: {KLRG}\n{G}\n{R}"
+        assert KLRG <= 0, f"Kullback-Leibler divergence less than zero: {KLRG}\n{G}\n{R}"
 
         if KLRG in KLRGs and KLRG != KLRG_prev:
             # We only log and break when we're at the best KLRG value of the
@@ -656,9 +656,9 @@ def var_mix_bound(G: np.ndarray, R: np.ndarray, V: np.ndarray,
         L_M2q = np.sum(
             R * np.nan_to_num(np.log(G / R), nan=0, posinf=0, neginf=0))
     # This fixes(?) some numerical problems.
-    # if L_M2q < 0 and np.isclose(L_M2q, 0):
-    #     L_M2q = 0
-    assert L_M2q >= 0, f"Kullback-Leibler divergence less than zero: {L_M2q}"
+    if L_M2q > 0 and np.isclose(L_M2q, 0):
+        L_M2q = 0
+    assert L_M2q <= 0, f"Kullback-Leibler divergence less than zero: {L_M2q}"
     # TODO Performance: slogdet can be cached, is computed more than once
     L_M3q = 0.5 * np.linalg.slogdet(Lambda_V_1)[1] + K * D_V / 2
     return L_M1q + L_M2q + L_M3q