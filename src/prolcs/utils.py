95ec89bd83c280bb5ab6d9f6a1665eb43c589216
David Pätzel <david.paetzel@informatik.uni-augsburg.de>
2021-04-28 09:16:11 +0200

    Add utilities for Gaussian confidence interval balls

--- src/prolcs/utils.py
+++ src/prolcs/utils.py
@@ -1,6 +1,11 @@
 from functools import wraps
 from time import asctime, localtime, time
+
 import numpy as np  # type: ignore
+import scipy.stats as st  # type: ignore
+import scipy.special as sp  # type: ignore
+from sklearn.utils import check_random_state  # type: ignore
+import scipy.optimize as so  # type: ignore
 
 
 def logstartstop(f):
@@ -32,7 +37,6 @@ def get_ranges(X: np.ndarray):
     return np.vstack([np.min(X, axis=0), np.max(X, axis=0)]).T
 
 
-
 def add_bias(X: np.ndarray):
     """
     Prefixes each input vector (i.e. row) in the given input matrix with 1 for
@@ -45,3 +49,82 @@ def add_bias(X: np.ndarray):
     """
     N, D_X = X.shape
     return np.hstack([np.ones((N, 1)), X])
+
+
+def pr_in_sd1(r=1):
+    """
+    Expected percentage of examples falling within one standard deviation of a
+    one-dimensional Gaussian distribution. See ``pr_in_sd``.
+
+    Parameters
+    ----------
+    r : float
+        Radius (in multiples of standard deviation).
+    """
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.erf.html
+    return sp.erf(r / np.sqrt(2))
+
+
+def pr_in_sd2(r=1):
+    """
+    Expected percentage of examples falling within one standard deviation of a
+    two-dimensional Gaussian distribution. See ``pr_in_sd``.
+
+    Parameters
+    ----------
+    r : float
+        Radius (in multiples of standard deviation).
+    """
+    return 1 - np.exp(-(r**2) / 2)
+
+
+def pr_in_sd(n=3, r=1):
+    """
+    Expected percentage of examples falling within multiples of a standard
+    deviation of a multivariate Gaussian distribution.
+
+    Reference for the used formulae:
+    https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0118537 .
+
+    Parameters
+    ----------
+    n : positive int
+        Dimensionality of the Gaussian.
+    r : float greater than 1
+        Factor for standard deviation radius. Numerical issues(?) if radius too
+        close to zero. We probably never want require r < 1, though, so this is
+        probably fine.
+    """
+    if r < 1:
+        raise ValueError(f"r = {r} < 1 may result in numerical issues")
+
+    if n == 1:
+        return pr_in_sd1(r=r)
+    elif n == 2:
+        return pr_in_sd2(r=r)
+    elif n >= 3:
+        ci = pr_in_sd(n=n - 2, r=r) - (r / np.sqrt(2))**(n - 2) * np.exp(
+            -(r**2) / 2) / sp.gamma(n / 2)
+        if ci < 0 and np.isclose(ci, 0):
+            return 0
+        else:
+            return ci
+    else:
+        raise ValueError("n must be positive")
+
+
+pr_in_sd_ = np.vectorize(pr_in_sd)
+
+
+def radius_for_ci(n=3, ci=0.5):
+    """
+    Calculate how many standard deviations are required to fulfill the given
+    confidence interval for a multivariate Gaussian of the given dimensionality.
+    """
+    # Other than in this German Wikipedia article we actually need to use
+    # SciPy's inverse of the *lower* incomplete gamma function:
+    # https://de.wikipedia.org/wiki/Mehrdimensionale_Normalverteilung
+    return np.sqrt(2 * sp.gammaincinv(n / 2, ci))
+
+
+radius_for_ci_ = np.vectorize(radius_for_ci)
---
+++ tests/prolcs/test_utils.py
@@ -0,0 +1,46 @@
+import hypothesis.strategies as st  # type: ignore
+import numpy as np  # type: ignore
+from hypothesis import given  # type: ignore
+from prolcs.utils import pr_in_sd, radius_for_ci
+
+
+@given(st.integers(min_value=1, max_value=100),
+       st.floats(min_value=1, max_value=100))
+def test_pr_in_sd_is_pr(n, r):
+    ci = pr_in_sd(n, r)
+    assert 0 <= ci <= 1
+
+
+@given(st.integers(min_value=1, max_value=100),
+       st.floats(min_value=0.01, max_value=0.99))
+def test_radius_for_ci_finite(n, ci):
+    r_ = radius_for_ci(n, ci)
+    assert np.isfinite(r_)
+
+
+@given(
+    st.tuples(st.integers(min_value=1, max_value=100),
+              st.floats(min_value=1, max_value=10)).
+    filter(
+        # Only generate data resulting in valid confidence intervals
+        # and make sure that we don't run into numerical issues.
+        lambda x: 0.01 <= pr_in_sd(x[0], x[1]) <= 0.99))
+def test_radius_for_ci_inverse_pr_in_sd(nr):
+    n, r = nr
+
+    ci = pr_in_sd(n, r)
+
+    r_ = radius_for_ci(n, ci)
+
+    assert np.isclose(
+        r_ / r, 1, rtol=1e-1), (f"not inverse to pr_in_sd: ci = {ci} and thus "
+                                f"r' / r = {r_} / {r} = {r_ / r}")
+
+
+# TODO DRY test_radial
+@st.composite
+def seeds(draw):
+    # Highest possible seed is `2**32 - 1` for NumPy legacy generators.
+    return draw(st.integers(min_value=0, max_value=2**32 - 1))
+
+
