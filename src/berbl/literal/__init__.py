Fix numerical issue in ΔL_k(q) assertion

src/berbl/literal/__init__.py
@@ -224,7 +224,7 @@ def train_classifier(m_k, X, Y):
         # (…). If this is not the case, then the implementation is faulty and/or
         # suffers from numerical instabilities.” [PDF p. 237]
         # TODO Consider extracting this to a test
-        assert delta_L_k_q >= 0, (
+        assert delta_L_k_q >= 0 or np.isclose(delta_L_k_q, 0), (
             f"iteration: {i}; Δ L_k(q) = {delta_L_k_q}; L_k(q) = {L_k_q}")
     return W_k, Lambda_k_1, a_tau_k, b_tau_k, a_alpha_k, b_alpha_k
 