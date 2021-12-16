149c1d8ffeed59813b560de0a1af92a15e05a332
David Pätzel <david.paetzel@uni-a.de>
2021-12-16 09:33:29 +0100

    Use pyproject.toml/setup.cfg packaging scheme

--- flake.nix
+++ flake.nix
@@ -1,20 +1,25 @@
 {
-  description = "The berbl library";
+  description = "The berbl Python library";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
   inputs.overlays.url = "github:dpaetzel/overlays";
 
-  outputs = { self, nixpkgs, overlays }: {
+  outputs = { self, nixpkgs, overlays }:
 
-    defaultPackage.x86_64-linux = with import nixpkgs {
+    with import nixpkgs {
       system = "x86_64-linux";
       overlays = with overlays.overlays; [ mlflow pandas ];
     };
+    rec {
+    defaultPackage.x86_64-linux =
       python3.pkgs.buildPythonPackage rec {
         pname = "berbl";
         version = "0.1.0";
 
         src = self;
+
+        # We use pyproject.toml.
+        format = "pyproject";
 
         propagatedBuildInputs = with python3.pkgs; [
           deap
@@ -37,5 +42,12 @@
           license = licenses.gpl3;
         };
       };
+
+    devShell.x86_64-linux = mkShell {
+      packages = [
+        defaultPackage.x86_64-linux
+        python3.pkgs.tox
+      ];
+    };
   };
 }
